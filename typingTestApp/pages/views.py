f2d2100e31290fa093c5ed9eec86eb8f749f403f
Patten Whiting <whi20045@byui.edu>
2021-03-09 14:18:17 -0700

    added queries

--- typingTestApp/pages/views.py
+++ typingTestApp/pages/views.py
@@ -1,9 +1,19 @@
 from django.views.generic import TemplateView
+from .models import Text
+from django.shortcuts import render, redirect
 
 # Create your views here.
 class HomePageView(TemplateView):
     template_name = 'home.html'
+    
+    def get(self,request):
+        item = Text.objects.get(id=1)
+        context = {
+            'text': item.text
+        }
+        return render(request, "../templates/home.html", context)
 
 class AboutPageView(TemplateView):
     template_name = 'about.html'
+
 
